7a8a5caa8a06b04b09f52c8c4bd9aa2e0a2be13c
Mateo1907 <mateusz.prasal@gmail.com>
2018-11-06 15:26:22 +0100

    Added another graphql package

--- config/app.php
+++ config/app.php
@@ -146,6 +146,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
 
         /*
          * Package Service Providers...
@@ -159,6 +160,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Rebing\GraphQL\GraphQLServiceProvider::class,
 
     ],
 
@@ -208,7 +210,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'GraphQL' => Nuwave\Lighthouse\Support\Facades\GraphQLFacade::class,
+        'GraphQL' => Rebing\GraphQL\Support\Facades\GraphQL::class,
     ],
 
 ];
